e3047b5877b2d3fc1e887d06fcd9cff5dfb3428c
Henry Hoffman <henry.hoffman@newfangled.games>
2021-07-01 22:56:54 +0100

    initial commit

---
+++ src/customUI.ts
@@ -0,0 +1,7 @@
+import * as ui from '@dcl/ui-scene-utils';
+
+export class CustomUI {
+    public ShowPrompt = () => {
+        var purchasePrompt = new ui.CustomPrompt(ui.PromptStyles.LIGHT, undefined, undefined, false)
+    }
+}
